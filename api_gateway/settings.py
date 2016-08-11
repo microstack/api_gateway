3ef117af78cbfbcddd5788693dfce40358b4d172
wikibootup <wikibootup@gmail.com>
2016-08-12 00:41:29 +0900

    set env var fron env files in settings

--- api_gateway/settings.py
+++ api_gateway/settings.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 import os
 
 # ex :) MOVIES_BACKEND_BASE_URL = 'http://localhost:8080'
-MOVIES_BASE_URL = os.environ.get('MOVIES_BASE_URL')
-POLITICS_BASE_URL = os.environ.get('POLITICS_BASE_URL')
+BACKEND_MOVIES_BASE_URL = os.environ.get('BACKEND_MOVIES_BASE_URL')
+BACKEND_POLITICS_BASE_URL = os.environ.get('BACKEND_POLITICS_BASE_URL')
+
+API_GW_PORT =  os.environ.get('API_GW_PORT') or 5000
+BACKEND_MOVIES_PORT = os.environ.get('BACKEND_MOVIES_PORT') or 80
+BACKEND_POLITICS_PORT = os.environ.get('BACKEND_POLITICS_PORT') or 80
+
+BACKEND_POLITICS_API_KEY_QUERY = os.environ.get(
+    'BACKEND_POLITICS_API_KEY_QUERY')
