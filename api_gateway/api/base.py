2a3eaffd4e4b1ca679657f982ff8fb9eeb5647d5
wikibootup <wikibootup@gmail.com>
2016-08-12 18:31:55 +0900

    add case exception

--- api_gateway/api/base.py
+++ api_gateway/api/base.py
@@ -8,6 +8,13 @@ class BaseForAPIGW():
 
     def response_text_from_request(self, resource):
         request_url = self.base_url + resource + self.api_key_query
-        response = requests.get(request_url)
+        try:
+            response = requests.get(request_url)
+        except requests.ConnectionError:
+            return 'ConnectionError'
+
+        if response.status_code != 200:
+            return 'Statuscode : {0}'.format(response.status_code)
+
         text = response.text
         return text
